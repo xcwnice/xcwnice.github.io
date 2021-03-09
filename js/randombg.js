31ddb267358e1696c0cdaeda33d8e375377a3060
findmefindyou <8779916+xcwnice@user.noreply.gitee.com>
2021-03-09 22:31:01 +0800

    Site updated: 2021-03-09 22:31:01

--- js/randombg.js
+++ js/randombg.js
@@ -1,9 +1,9 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(https://i.loli.net/2021/03/09/I5o3NDn4V7f6cwJ.jpg)",
-  "url(https://i.loli.net/2021/03/09/LtEoKTdSDku1v8Q.jpg)",
-  "url(https://i.loli.net/2021/03/09/gGfYkOB15EPKjv8.jpg)",
-  "url(https://i.loli.net/2021/03/09/5Y91PX6oLznCjr7.jpg)"
+  "url(/img/xcw/12.jpg)",
+  "url(/img/xcw/23.jpg)",
+  "url(/img/xcw/34.jpg)",
+  "url(/img/xcw/56.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));

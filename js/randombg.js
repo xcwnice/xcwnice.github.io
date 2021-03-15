b3b34e5ebe96c91d25a828ae2eb6e1e0e88b0b03
xcwnice <3014887991@qq.com>
2021-03-15 20:24:49 +0800

    Site updated: 2021-03-15 20:24:39

--- js/randombg.js
+++ js/randombg.js
@@ -3,7 +3,8 @@ var backimg =[
   "url(/img/xcw/12.jpg)",
   "url(/img/xcw/23.jpg)",
   "url(/img/xcw/34.jpg)",
-  "url(/img/xcw/56.jpg)"
+  "url(/img/xcw/56.jpg)",
+  "url(/img/xcw/bg1.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));

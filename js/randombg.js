8c039c9b94c0f7a19198d45dca3c4a7071fc7622
xcwnice <3014887991@qq.com>
2021-03-15 22:08:41 +0800

    Site updated: 2021-03-15 22:08:30

--- js/randombg.js
+++ js/randombg.js
@@ -1,10 +1,17 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/img/xcw/12.jpg)",
-  "url(/img/xcw/23.jpg)",
   "url(/img/xcw/34.jpg)",
   "url(/img/xcw/56.jpg)",
-  "url(/img/xcw/bg1.jpg)"
+  "url(/img/xcw/bg1.jpg)",
+  "url(/img/xcw/bg2.jpg)",
+  "url(/img/xcw/bg4.jpg)",
+  "url(/img/xcw/bg6.jpg)",
+  "url(/img/xcw/bg7.jpg)",
+  "url(/img/xcw/bg8.jpg)",
+  "url(/img/xcw/bg9.jpg)",
+  "url(/img/xcw/bg10.jpg)",
+  "url(/img/xcw/bg11.jpg)",
+  "url(/img/xcw/bg12.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));
